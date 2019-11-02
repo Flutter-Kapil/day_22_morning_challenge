af64bc6a9b51c6918b251c1dd8308965a134ac26
kapil jhajhria <jhajhria44@gmail.com>
2019-11-02 11:59:27 +0530

    save 01

--- bin/main.dart
+++ bin/main.dart
@@ -25,6 +25,54 @@
 //    [3, 9, 4, 9],
 //    [9, 9, 3, 1],
 //  ]
+List<List> minesweeper(List<List> board) {
+  for (int i = 0; i < board.length; i++) {
+    for (int j = 0; j < board[i].length; j++) {
+      if (board[i][j] == 1) {
+        board[i][j] = 9;
+      }
+    }
+  }
+  for (int i = 0; i < board.length; i++) {
+    for (int j = 0; j < board[i].length; j++) {
+      if (board[i][j] == 9) {
+        nearby9(i, j, board);
+      }
+    }
+  }
+  return board;
+}
+
+void nearby9(int i, int j, List<List> board) {
+  try {
+    if (i - 1 != null && j - 1 != null && board[i - 1][j - 1] != 9) {
+      board[i - 1][j - 1] = board[i - 1][j - 1] + 1;
+    }
+    if (i - 1 != null && j != null && board[i - 1][j] != 9) {
+      board[i - 1][j] = board[i - 1][j] + 1;
+    }
+    if (i - 1 != null && j + 1 != null && board[i - 1][j + 1] != 9) {
+      board[i - 1][j + 1] = board[i - 1][j + 1] + 1;
+    }
+    if (i != null && j - 1 != null && board[i][j - 1] != 9) {
+      board[i][j - 1] = board[i][j - 1] + 1;
+    }
+    if (i != null && j + 1 != null && board[i][j + 1] != 9) {
+      board[i][j + 1] = board[i][j + 1] + 1;
+    }
+    if (i + 1 != null && j - 1 != null && board[i + 1][j - 1] != 9) {
+      board[i + 1][j - 1] = board[i + 1][j - 1] + 1;
+    }
+    if (i + 1 != null && j != null && board[i + 1][j] != 9) {
+      board[i + 1][j] = board[i][j] + 1;
+    }
+    if (i + 1 != null && j + 1 != null && board[i + 1][j + 1] != 9) {
+      board[i + 1][j + 1] = board[i + 1][j + 1] + 1;
+    }
+  } catch (e) {
+    print(e);
+  }
+}
 
 // Challenge 3
 // Group in Order
@@ -41,7 +89,37 @@
 //  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], 4
 //  [[1, 4, 7, 10], [2, 5, 8, 11], [3, 6, 9]]
 /// "Leftover" arrays are okay.
+List<List> divideList(List initialList, int x) {
+  List<List> finalList = [];
+  int totalSubList = (initialList.length ~/ x) + (initialList.length % x);
+  for (int i = 0; i < totalSubList; i++) {
+    finalList.add([]);
+  }
+  //now we have list of list with empty sub Lists
+  int subListNumber = 0;
+  for (int i = 0; i < (initialList.length - initialList.length % x); i++) {
+    for (int j = 0; j < x; j++) {
+      finalList[subListNumber].add(finalList[i]);
+    }
+    subListNumber++;
+  }
+  int remainderElements = initialList.length % x;
+  if (remainderElements != 0) {
+    for (int i = (finalList.length - remainderElements);
+        i < finalList.length;
+        i++) {
+      finalList[subListNumber].add(i);
+    }
+  }
+  return finalList;
+}
 
 main() {
-
+//  print(minesweeper([
+//    [0, 1, 0, 0],
+//    [0, 0, 1, 0],
+//    [0, 1, 0, 1],
+//    [1, 1, 0, 0],
+//  ]));
+  print(divideList([1, 2, 3, 4, 5, 6], 3));
 }
